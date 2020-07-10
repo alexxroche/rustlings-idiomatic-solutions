e8b6c80a6a6f870cfaba404020823739a053c686
Alexx Roche <notice-dev@alexx.net>
2020-07-10 17:43:48 +0200

    functions1.rs solution

---
+++ exercises/functions/functions1.rs_/functions1.rs
@@ -0,0 +1,12 @@
+// functions1.rs
+// Make me compile! Execute `rustlings hint functions1` for hints :)
+
+// I AM NOT DONE
+
+fn call_me() -> () {
+  println!("Called");
+}
+
+fn main() {
+    call_me();
+}
