f1ffa2403afa50b00b35a7bd3334974e51b94963
Alexx Roche <notice-dev@alexx.net>
2020-07-13 17:15:07 +0200

    quiz4.rs solution

---
+++ exercises/quiz4.rs_/quiz4.rs
@@ -0,0 +1,31 @@
+// quiz4.rs
+// This quiz covers the sections:
+// - Modules
+// - Macros
+
+// Write a macro that passes the quiz! No hints this time, you can do it!
+
+// I AM NOT DONE
+
+macro_rules! my_macro {
+    ($val:expr) => {
+       format!("Hello {}", $val)
+       //  String::from("Hello ") + $val  // This is also a valid solution
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_my_macro_world() {
+        assert_eq!(my_macro!("world!"), "Hello world!");
+    }
+
+    #[test]
+    fn test_my_macro_goodbye() {
+        assert_eq!(my_macro!("goodbye!"), "Hello goodbye!");
+    }
+}
