b1d034b7603ea1915f11784a6a91c6db36705d07
Alexx Roche <notice-dev@alexx.net>
2020-07-13 16:23:10 +0200

    enums2.rs solution

---
+++ exercises/enums/enums2.rs_/enums2.rs
@@ -0,0 +1,31 @@
+// enums2.rs
+// Make me compile! Execute `rustlings hint enums2` for hints!
+
+// I AM NOT DONE
+
+#[derive(Debug)]
+enum Message {
+    Move{x: i32, y: i32},
+    Echo(String),
+    ChangeColor(u8,u8,u8), //i32 is equally valid
+    Quit,
+}
+
+impl Message {
+    fn call(&self) {
+        println!("{:?}", &self);
+    }
+}
+
+fn main() {
+    let messages = [
+        Message::Move{ x: 10, y: 30 },
+        Message::Echo(String::from("hello world")),
+        Message::ChangeColor(200, 255, 255),
+        Message::Quit
+    ];
+
+    for message in &messages {
+        message.call();
+    }
+}
