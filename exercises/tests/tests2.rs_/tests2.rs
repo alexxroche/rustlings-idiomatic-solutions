57c4910a7f2439d93df8cd1c0cd3a2ff5769b61e
Alexx Roche <notice-dev@alexx.net>
2020-07-13 16:35:40 +0200

    tests2.rs solutions

---
+++ exercises/tests/tests2.rs_/tests2.rs
@@ -0,0 +1,20 @@
+// tests2.rs
+// This test has a problem with it -- make the test compile! Make the test
+// pass! Make the test fail! Execute `rustlings hint tests2` for hints :)
+
+// I AM NOT DONE
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn you_can_assert_eq() {
+        assert_eq!(1,1);
+
+        // Each of the following are also valid solutions for this exercise
+
+        //assert_eq!(true,true);
+        //assert_eq!(false,false);
+        //assert_eq!("b","b");
+
+    }
+}
