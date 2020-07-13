337764956f8146c577e84b48071d673b397abfc1
Alexx Roche <notice-dev@alexx.net>
2020-07-13 16:11:03 +0200

    quiz2.rs solution

---
+++ exercises/quiz2.rs_/quiz2.rs
@@ -0,0 +1,30 @@
+// quiz2.rs
+// This is a quiz for the following sections:
+// - Strings
+
+// Ok, here are a bunch of values-- some are `String`s, some are `&str`s. Your
+// task is to call one of these two functions on each value depending on what
+// you think each value is. That is, add either `string_slice` or `string`
+// before the parentheses on each line. If you're right, it will compile!
+
+// I AM NOT DONE
+
+fn string_slice(arg: &str) {
+    println!("{}", arg);
+}
+fn string(arg: String) {
+    println!("{}", arg);
+}
+
+fn main() {
+    string_slice("blue");
+    string("red".to_string());
+    string(String::from("hi"));
+    string("rust is fun!".to_owned());
+    string("nice weather".into());
+    string(format!("Interpolation {}", "Station"));  // This might not be obvious
+    string_slice(&String::from("abc")[0..1]);
+    string_slice("  hello there ".trim());           // This one can also be challenging
+    string("Happy Monday!".to_string().replace("Mon", "Tues"));
+    string("mY sHiFt KeY iS sTiCkY".to_lowercase());
+}
