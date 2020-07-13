34736cfaf8f9653289061a1aba3678e71cfde35b
Alexx Roche <notice-dev@alexx.net>
2020-07-13 17:03:07 +0200

    macros3.rs solutions

---
+++ exercises/macros/macros3.rs_/macros3_b.rs
@@ -0,0 +1,18 @@
+// macros3.rs
+// Make me compile, without taking the macro out of the module!
+// Execute `rustlings hint macros3` for hints :)
+
+// I AM NOT DONE
+
+#[macro_use]
+mod macros {
+    macro_rules! my_macro {
+        () => {
+            println!("Check out my macro!");
+        };
+    }
+}
+
+fn main() {
+    my_macro!();
+}
