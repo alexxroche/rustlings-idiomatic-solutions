dc29f5e9229d390f9a35d0d3a31848fcefca6662
Alexx Roche <notice-dev@alexx.net>
2020-07-13 16:05:24 +0200

    strings1.rs solution

---
+++ exercises/strings/strings1.rs_/strings1.rs
@@ -0,0 +1,18 @@
+// strings1.rs
+// Make me compile without changing the function signature!
+// Execute `rustlings hint strings1` for hints ;)
+
+// I AM NOT DONE
+
+fn main() {
+    let answer = current_favorite_color();
+    println!("My current favorite color is {}", answer);
+}
+
+fn current_favorite_color() -> String {
+    "blue".to_string()
+
+    // Either of these two will also solve this exercise
+    //String::from("blue")
+    //format!("blue")
+}
