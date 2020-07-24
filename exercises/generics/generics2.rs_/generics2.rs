81612e6953b66a73f8ea3f3a986e10306ed7cf26
Alexx Roche <notice-dev@alexx.net>
2020-07-24 19:24:59 +0200

    generics2.rs solution

---
+++ exercises/generics/generics2.rs_/generics2.rs
@@ -0,0 +1,28 @@
+// This powerful wrapper provides the ability to store a positive integer value.
+// Rewrite it using generics so that it supports wrapping ANY type.
+
+// I AM NOT DONE
+struct Wrapper<T> {
+    value: T
+}
+
+impl<T> Wrapper<T> {
+    pub fn new(value: T) -> Self {
+        Wrapper { value }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn store_u32_in_wrapper() {
+        assert_eq!(Wrapper::new(42).value,  42);
+    }
+
+    #[test]
+    fn store_str_in_wrapper() {
+        assert_eq!(Wrapper::new("Foo").value, "Foo");
+    }
+}
