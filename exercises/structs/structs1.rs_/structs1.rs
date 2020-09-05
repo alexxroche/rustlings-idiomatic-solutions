fc55033d6d706f08637b1a62797edcd52d189257
Neal McBurnett <neal@mcburnett.org>
2020-09-05 17:47:35 -0600

    structs1.rs with String ala Rust book
    Currently, the Rust Book doesn't introduce lifetimes until Chapter 10,
    so the current solution with &'a str is quite foreign for students
    following it.
    
    So, for people who are using the Book, here is an example that follows it.
    
    I just noticed that the structs README actually points to the
    chapter on structs from Rust by Example
    
     https://doc.rust-lang.org/rust-by-example/custom_types/structs.html
    
    which notes the use of lifetimes without really discussing them, just providing
    a link to the chapter on them.

---
+++ exercises/structs/structs1.rs_/structs1.rs
@@ -0,0 +1,46 @@
+// structs1.rs
+// Address all the TODOs to make the tests pass!
+
+// I AM NOT DONE
+
+struct ColorClassicStruct {
+    name: String,
+    hex: String,
+}
+
+struct ColorTupleStruct(String, String);
+
+#[derive(Debug)]
+struct UnitStruct;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn classic_c_structs() {
+        // Instantiate a classic c struct!
+        let green = ColorClassicStruct{name:String::from("green"), hex:String::from("#00FF00")};
+
+        assert_eq!(green.name, "green");
+        assert_eq!(green.hex, "#00FF00");
+    }
+
+    #[test]
+    fn tuple_structs() {
+        // TODO: Instantiate a tuple struct!
+        let green = ColorTupleStruct(String::from("green"), String::from("#00FF00"));
+
+        assert_eq!(green.0, "green");
+        assert_eq!(green.1, "#00FF00");
+    }
+
+    #[test]
+    fn unit_structs() {
+        // Instantiate a unit struct!
+        let unit_struct = UnitStruct;
+        let message = format!("{:?}s are fun!", unit_struct);
+
+        assert_eq!(message, "UnitStructs are fun!");
+    }
+}
